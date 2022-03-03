c8c873c561c54630e4fc1537cdc6459799b4ece6
AlexUtopia <interminatus.utopia@gmail.com>
2022-03-04 01:03:01 +0300

    - продолжаю разработку, пока не запускал.

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import re
 import subprocess
 import json
 import ipaddress
+import time
 import urllib.request
 import urllib.parse
+
+WATCH_DOG_CLIENT_CONFIG = "watchdog-client.config"
+SLEEP_BEFORE_RECONNECT_SEC = 30
 
 
 # https://tproger.ru/translations/demystifying-decorators-in-python/
@@ -28,6 +32,10 @@ class TextConfigWriter:
     def __init__(self, config_file_path, encoding="utf-8"):
         self.__config_file_path = str(config_file_path)
         self.__encoding = str(encoding)
+
+    def set(self, data):
+        with open(self.__config_file_path, mode="wt", encoding=self.__encoding) as config_file:
+            config_file.write(data)
 
 
 class JsonConfigLoader:
@@ -137,12 +145,9 @@ class MyExternalIpAddressAndPort:
 class OpenVpnServer:
     OPEN_VPN = "openvpn"
 
-    def __init__(self, local_port, config_file_path=None):
+    def __init__(self, local_port, config_file_path="./openvpn-server.config"):
         self.__local_port = local_port
-        if config_file_path is None:
-            self.__config_file_path = "./openvpn-server.config"
-        else:
-            self.__config_file_path = str(config_file_path)
+        self.__config_file_path = str(config_file_path)
 
     def run(self):
         result = subprocess.run(
@@ -156,6 +161,27 @@ class OpenVpnServer:
             raise Exception("OpenVpn server start FAIL: {}".format(result.stderr))
 
         print("OpenVpn server start OK")
+
+
+class OpenVpnClient:
+    OPEN_VPN = "openvpn"
+
+    def __init__(self, local_port, config_file_path="./{}".format(WATCH_DOG_CLIENT_CONFIG)):
+        self.__local_port = local_port
+        self.__config_file_path = str(config_file_path)
+
+    def run(self):
+        result = subprocess.run(
+            [self.OPEN_VPN, "--config", self.__config_file_path],
+            capture_output=True,
+            text=True
+        )
+        print("OpenVpn client return: {}".format(result))
+
+        if result.returncode:
+            raise Exception("OpenVpn client start FAIL: {}".format(result.stderr))
+
+        print("OpenVpn client start OK")
 
 
 class TelegramBotConfig(JsonConfigLoader):
@@ -208,14 +234,15 @@ class TelegramClient:
 #         print("Stun client return: {}".format(result))
 
 
-# Читаем с конфига где брать пути до настроек OpenVpn
-
 class OpenVpnConfig:
     def __init__(self, open_vpn_config_path="./open-vpn.config.json"):
-        self.__config_reader = JsonConfigLoader(open_vpn_config_path)
+        self.__config_reader = JsonConfigLoader(str(open_vpn_config_path))
 
     def get_server_name(self):
         return str(self.__config_reader.get()["open_vpn_server_name"])
+
+    def get_server_port(self):
+        return str(self.__config_reader.get()["open_vpn_server_port"])
 
     def get_config_dir(self):
         return str(self.__config_reader.get()["open_vpn_config_dir"])
@@ -233,15 +260,15 @@ class OpenVpnConfig:
         return "{}/ta.key".format(self.get_config_dir())
 
     def get_ca_cert(self):
-        return self.__load_xxx(TextConfigReader(self.get_ca_cert_path()).get(), "-----BEGIN CERTIFICATE-----",
-                               "-----END CERTIFICATE-----")
+        return self.__parse(TextConfigReader(self.get_ca_cert_path()).get(), "-----BEGIN CERTIFICATE-----",
+                            "-----END CERTIFICATE-----")
 
     def get_tls_auth_key(self):
-        return self.__load_xxx(TextConfigReader(self.get_tls_auth_key_path()).get(),
-                               "-----BEGIN OpenVPN Static key V1-----",
-                               "-----END OpenVPN Static key V1-----")
+        return self.__parse(TextConfigReader(self.get_tls_auth_key_path()).get(),
+                            "-----BEGIN OpenVPN Static key V1-----",
+                            "-----END OpenVPN Static key V1-----")
 
-    def __load_xxx(self, config_as_string, begin_label, end_label):
+    def __parse(self, config_as_string, begin_label, end_label):
         regex = re.compile(r"({}[\s\S]*{})".format(begin_label, end_label), re.MULTILINE)
         if not regex.match():
             print("[W] config_as_string: {}".format(config_as_string))
@@ -252,7 +279,7 @@ class OpenVpnConfig:
 
 class OpenVpnClientConfig(OpenVpnConfig):
     def __init__(self, user_name, open_vpn_config_path="./open-vpn.config.json"):
-        super().__init__(open_vpn_config_path)
+        super().__init__(str(open_vpn_config_path))
         self.__user_name = user_name
 
     pass
@@ -264,30 +291,71 @@ class OpenVpnClientConfig(OpenVpnConfig):
         return "{}/{}.key".format(self.get_client_config_dir(), self.__user_name)
 
     def get_client_cert(self):
-        return self.__load_xxx(TextConfigReader(self.get_client_cert_path()).get(), "-----BEGIN CERTIFICATE-----",
-                               "-----END CERTIFICATE-----")
+        return self.__parse(TextConfigReader(self.get_client_cert_path()).get(), "-----BEGIN CERTIFICATE-----",
+                            "-----END CERTIFICATE-----")
 
     def get_client_key(self):
-        return self.__load_xxx(TextConfigReader(self.get_client_key_path()).get(), "-----BEGIN PRIVATE KEY-----",
-                               "-----END PRIVATE KEY-----")
+        return self.__parse(TextConfigReader(self.get_client_key_path()).get(), "-----BEGIN PRIVATE KEY-----",
+                            "-----END PRIVATE KEY-----")
+
+    # fixme utopia Получить весь список пользователей
+    # fixme utopia Сохранять вместе с логином пользователя telegram
 
 
-# https://stackoverflow.com/questions/17493307/creating-set-of-objects-of-user-defined-class-in-python
-class OpenVpnConfigLine:
+class OpenVpnConfigKeyValue:
+    def __init__(self):
+        self.__container = {}
 
+    def add(self, key, value):
+        as_xml = False
+        self.__add(key, value, as_xml)
+
+    def add_as_xml(self, key, value):
+        as_xml = True
+        self.__add(key, value, as_xml)
+
+    def render(self):
+        result = ""
+        for key, value in self.__container.items():
+            result += self.__render_value(key, value)
+        return result
+
+    def __add(self, key, value, as_xml):
+        if value is None:
+            self.__container[str(key)] = {"value": None, "as_xml": as_xml}
+        else:
+            self.__container[str(key)] = {"value": str(value), "as_xml": as_xml}
+
+    def __render_value(self, key, value):
+        if bool(value.is_xml):
+            if value.value is None:
+                result = "<{0}></{0}>\n".format(key)
+            else:
+                result = "<{0}>\n{1}\n</{0}>\n".format(key, value)
+        else:
+            if value.value is None:
+                result = "{}\n".format(key)
+            else:
+                result = "{} {}\n".format(key, value)
+        return result
 
 
 class OpenVpnClientConfigGenerator:
     def __init__(self, ip_address_and_port, user_name,
-                 client_config_template_file_path="./HomeVpn-client.config.template"):
-        self.__config_reader = TextConfigReader(client_config_template_file_path)
-        self.__config = {}
+                 client_config_template_file_path="./open-vpn-client.config.template", output_client_config_dir="."):
+        self.__client_config_template_reader = TextConfigReader(client_config_template_file_path)
+        self.__key_value_config = OpenVpnConfigKeyValue()
         self.__open_vpn_client_config = OpenVpnClientConfig(user_name)
+        self.__output_client_config_file = TextConfigWriter(
+            "{}/{}.ovpn".format(str(output_client_config_dir), user_name))
         self.__ip_address_and_port = ip_address_and_port
+
+    def generate(self):
+        self.__output_client_config_file.set(self.__render_to_string())
 
     def __render_to_string(self):
         self.__generate()
-        return ""
+        return self.__key_value_config.render()
 
     def __generate(self):
         self.__parse_template()
@@ -300,46 +368,64 @@ class OpenVpnClientConfigGenerator:
         self.__add_client_key()
 
     def __parse_template(self):
-        # (<[a-z]+>)[\s\S]*?(<\/[a-z]+>) Парсинг унифицированного формата
-
         regex = re.compile(r"^[ \t]*([a-z\-_0-9]+)[ \t]*(.*)\n", re.MULTILINE)
-        tmp = regex.findall(self.__config_reader.get())
+        tmp = regex.findall(self.__client_config_template_reader.get())
 
         if len(tmp) == 0:
-            raise Exception("Parse ip_address_and_port FAIL")
+            raise Exception("Parse ip_address_and_port FAIL")  # fixme utopia text
 
         for t in tmp:
-            self.__config[t[0]] = t[1]
+            self.__key_value_config.add(t[0], t[1])
 
     def __add_ip_address(self):
-        self.__config["remote"] = self.__ip_address_and_port.get_ip_address()
+        self.__key_value_config.add("remote", self.__ip_address_and_port.get_ip_address())
 
     def __add_port(self):
-        self.__config["port"] = self.__ip_address_and_port.get_port()
+        self.__key_value_config.add("port", self.__ip_address_and_port.get_port())
 
     def __add_key_direction(self):
-        self.__config["key-direction"] = "1"
+        self.__key_value_config.add("key-direction", 1)
 
     def __add_ca_cert(self):
-        self.__config["<ca>"] = self.__open_vpn_client_config.get_ca_cert()
+        self.__key_value_config.add_as_xml("ca", self.__open_vpn_client_config.get_ca_cert())
 
     def __add_tls_auth_key(self):
-        self.__config["<tls-auth>"] = self.__open_vpn_client_config.get_tls_auth_key()
+        self.__key_value_config.add_as_xml("tls-auth", self.__open_vpn_client_config.get_tls_auth_key())
 
     def __add_client_cert(self):
-        self.__config["<cert>"] = self.__open_vpn_client_config.get_client_cert()
+        self.__key_value_config.add_as_xml("cert", self.__open_vpn_client_config.get_client_cert())
 
     def __add_client_key(self):
-        self.__config["<cert>"] = self.__open_vpn_client_config.get_client_key()
+        self.__key_value_config.add_as_xml("key", self.__open_vpn_client_config.get_client_key())
 
 
-open_vpn_local_port = 35000
+open_vpn_server_port = OpenVpnConfig().get_server_port()
 
-stun_client22 = MyExternalIpAddressAndPort(open_vpn_local_port)
-my_ip_address_and_port = stun_client22.get()
+# fixme utopia Конфигурацию сервера нужно генерировать из шаблона и подсовывать серверу ovpn файл
 
-TelegramClient().send_message(
-    "Хорошего дня, лови новые параметры подключения\nIP Address: {}\nPort: {}".format(
-        my_ip_address_and_port.get_ip_address(), my_ip_address_and_port.get_port()))
+OpenVpnServer(open_vpn_server_port).run()
 
-OpenVpnServer(open_vpn_local_port).run()
+while True:
+    my_ip_address_and_port = MyExternalIpAddressAndPort(open_vpn_server_port).get()
+
+    TelegramClient().send_message(
+        "Хорошего дня, лови новые параметры подключения\nIP Address: {}\nPort: {}".format(
+            my_ip_address_and_port.get_ip_address(), my_ip_address_and_port.get_port()))
+
+    OpenVpnClientConfigGenerator(my_ip_address_and_port, WATCH_DOG_CLIENT_CONFIG).generate()
+
+    # fixme utopia Сгенерировать новые ovpn для всех клиентов и разослать их всем клиентам telegram бота
+    #              список чатов видимо придётся копить в каком-то локальном конфиге, т.к. у телеграма нет такого метода в api
+
+    try:  # fixme utopia Перепроверить что мы можем засечь разрыв соединения, к примеру, выключить WiFi
+        OpenVpnClient(WATCH_DOG_CLIENT_CONFIG).run()
+    except Exception:
+        print("Try udp hole punching and RECONNECT")
+
+    time.sleep(SLEEP_BEFORE_RECONNECT_SEC)
+
+# fixme utopia Выставить main для получения данных с open-vpn.config.json
+#              Скрипты build-client.sh и setup-easy-rsa.sh должны использовать open-vpn.config.json
+
+# fixme utopia Telegram бот в отдельный поток для обслуживания пользователей, работаем в режиме long polling
+#              my_ip_address_and_port сделать шареной между потоками переменной
