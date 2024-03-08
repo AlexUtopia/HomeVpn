1e005fe95d7aaadccc2a5a51e380e40da5ec6177
AlexUtopia <interminatus.utopia@gmail.com>
2024-03-09 02:28:47 +0300

    - ссылка на основополагающую статью для скрипта connect_to_host_over_ssh.sh

--- connect_to_host_over_ssh.sh
+++ connect_to_host_over_ssh.sh
@@ -3,5 +3,6 @@
 HOST_IP_ADDRESS="192.168.0.148"
 USER="utopia"
 
+# https://www.simplified.guide/ssh/x11-forwarding-as-root
 
 ssh -t -XC "${USER}"@"${HOST_IP_ADDRESS}" 'echo "Enter root password for run X11 over current ssh session from root user"; sudo xauth add `xauth list "${DISPLAY}"`; bash -l' || exit $?
