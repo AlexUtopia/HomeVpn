a363c3474d02f28436adde82e685d8310b14d079
AlexUtopia <interminatus.utopia@gmail.com>
2025-11-10 00:54:52 +0300

    - [начало] разбиение setup-packages.sh на группу библиотек.

---
+++ lib/bash/config.include.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+## @brief Переменные конфигурации разворота проекта
+
+source "${PROJECT_ROOT}/lib/bash/os.include.sh"
+
+## @brief Конфигурация пакетов
+GLOBAL_CONFIG_SETUP_PACKAGES_MODE="full" # min, dev, full
+
+## @brief Конфигурация пути до корня файловой системы
+GLOBAL_CONFIG_ROOT_PREFIX=""
+if is_termux; then
+    GLOBAL_CONFIG_ROOT_PREFIX="${PREFIX}/.."
+fi
+
+## @brief Конфигурация пути до директории usr
+GLOBAL_CONFIG_USR_PREFIX="/usr"
+if is_termux; then
+    GLOBAL_CONFIG_USR_PREFIX="${PREFIX}"
+fi
+
+## @brief Конфигурация пути до директории etc
+GLOBAL_CONFIG_ETC_PREFIX="/etc"
+if is_termux; then
+    GLOBAL_CONFIG_ETC_PREFIX="${GLOBAL_CONFIG_USR_PREFIX}/etc"
+fi
+
+## @brief Имя пользователя от которого будет работать VNC сервер
+## @details В Android/termux переменная окружения USER не установлена
+GLOBAL_CONFIG_VNC_SERVER_USER=$(logname)
+if [[ -z "${GLOBAL_CONFIG_VNC_SERVER_USER}" ]]; then
+    echo "[FATAL] GLOBAL_CONFIG_VNC_USER parameter IS NOT SET"
+    exit 1
+fi
+
+## @brief Имя пользователя от которого будет работать SMB сервер
+## @details В Android/termux переменная окружения USER не установлена
+GLOBAL_CONFIG_SMB_SERVER_USER=$(logname)
+if [[ -z "${GLOBAL_CONFIG_SMB_SERVER_USER}" ]]; then
+    echo "[FATAL] GLOBAL_CONFIG_SMB_SERVER_USER parameter IS NOT SET"
+    exit 1
+fi
+
+## @brief Путь до shared директории SMB сервера
+GLOBAL_CONFIG_SMB_SERVER_PUBLIC_DIR_PATH="${GLOBAL_CONFIG_ROOT_PREFIX}/smb_share_public"
+if is_msys; then
+    GLOBAL_CONFIG_SMB_SERVER_PUBLIC_DIR_PATH="${SYSTEMDRIVE}/smb_share_public"
+fi
+
+## @brief TCP порты на которых работает SMB сервер
+## @details https://unlix.ru/%D0%BD%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0-%D1%84%D0%B0%D0%B5%D1%80%D0%B2%D0%BE%D0%BB%D0%B0-iptables-%D0%B4%D0%BB%D1%8F-samba/
+## @details Android не может использовать порты ниже 1024, см. https://android.stackexchange.com/a/205562
+GLOBAL_CONFIG_SMB_SERVER_TCP_PORTS="139 445"
+if is_termux; then
+    GLOBAL_CONFIG_SMB_SERVER_TCP_PORTS="1139 4445"
+fi
---
+++ lib/bash/os.include.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+## @brief Базовые функции получения информации о текущей ОС
+
+
+## @brief Проверить можно ли выполнить команду от имени администратора
+## @details Работает для Linux, Android/termux
+## @details su   : termux-tools / util-linux
+##          sudo : tsu / sudo
+## https://github.com/termux/termux-tools/blob/master/scripts/su.in
+## @retval 0 - можно выполнить команду от имени администратора, 1 - нет
+function is_admin_rights_available() {
+    su --version &> "/dev/null" || return $?
+    return 0
+}
+
+## @brief Получить название текущей ОС
+## @details Результат - строка в нижнем регистре
+## @return Название текущей ОС
+## @retval 0 - успешно, 1 - нет
+function get_os_name() {
+    local RESULT=""
+    RESULT=$(uname -o) || return $?
+    echo "${RESULT,,}"
+    return 0
+}
+
+## @brief Получить название архитектуры ОС
+## @details Результат - строка в нижнем регистре
+## @return Название архитектуры ОС
+## @retval 0 - успешно, 1 - нет
+function get_machine_name() {
+    local RESULT=""
+    RESULT=$(uname -m) || return $?
+    echo "${RESULT,,}"
+    return 0
+}
+
+## @brief Проверить является ли текущая ОС Linux
+## @retval 0 - текущая ОС Linux, 1 - нет
+function is_linux() {
+   if [[ "${OS_NAME}" == *"linux"* ]]; then
+       return 0
+   fi
+   return 1
+}
+
+## @brief Проверить является ли текущая ОС Android/termux
+## @retval 0 - текущая ОС Android/termux, 1 - нет
+function is_termux() {
+   if [[ "${OS_NAME}" == *"android"* ]]; then
+       return 0
+   fi
+   return 1
+}
+
+## @brief Проверить является ли текущая ОС Windows/MSYS2
+## @retval 0 - текущая ОС Windows/MSYS2, 1 - нет
+function is_msys() {
+   if [[ "${OS_NAME}" == *"msys"* ]]; then
+       return 0
+   fi
+   return 1
+}
+
+## @brief Проверить является ли текущая ОС Windows/Cygwin
+## @retval 0 - текущая ОС Windows/Cygwin, 1 - нет
+function is_cygwin() {
+   if [[ "${OS_NAME}" == *"cygwin"* ]]; then
+       return 0
+   fi
+   return 1
+}
+
+## @brief Получить название дистрибутива Linux
+## @details Работает для Linux
+## @details https://www.freedesktop.org/software/systemd/man/latest/os-release.html
+## @details Результат - строка в нижнем регистре
+## @return Название дистрибутива Linux
+## @retval 0 - успешно, 1 - нет
+function get_linux_distro_name() {
+   source "/etc/os-release" || return $?
+
+   if [[ -n "${UBUNTU_CODENAME}" ]]; then
+       # Для Ubuntu-based дистрибутивов linux (типа Linix Mint) всегда используются ubuntu PPA
+       echo "ubuntu"
+   else
+       echo "${ID,,}"
+   fi
+   return 0
+}
+
+## @brief Получить название версии дистрибутива Linux
+## @details Работает для Linux
+## @details https://www.freedesktop.org/software/systemd/man/latest/os-release.html
+## @details Результат - строка в нижнем регистре
+## @return Название версии дистрибутива Linux
+## @retval 0 - успешно, 1 - нет
+function get_linux_distro_codename_or_version() {
+   source "/etc/os-release" || return $?
+
+   if [[ -n "${UBUNTU_CODENAME}" ]]; then
+       echo "${UBUNTU_CODENAME,,}"
+   elif [[ -n "${VERSION_CODENAME}" ]]; then
+       echo "${VERSION_CODENAME,,}"
+   else
+       echo "${VERSION_ID,,}"
+   fi
+   return 0
+}
+
+## @brief Закэшированное имя текущей ОС
+OS_NAME=$(get_os_name)
+
+## @brief Закэшированная архитектура текущей ОС
+MACHINE_NAME=$(get_machine_name)
